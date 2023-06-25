d201570aae2b89a608194252ec56cd679fe4f5c6
adarsht0001 <adarsht00001@gmail.com>
2023-06-25 18:04:46 +0530

    signup implemeted

---
+++ src/app/api/signup/route.js
@@ -0,0 +1,35 @@
+import connect from "../../../utils/db";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+import User from "../../../models/User";
+
+export const POST = async (request) => {
+  const { name, email, password } = await request.json();
+  await connect();
+
+  const user = await User.findOne({ email: email });
+  if (user) {
+    return new NextResponse("user already exists", {
+      status: 500,
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 5);
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    return new NextResponse("User has been created", {
+      status: 201,
+    });
+  } catch (err) {
+    return new NextResponse(err.message, {
+      status: 500,
+    });
+  }
+};
